e46ebd0e0d8f8fe6299f07977c82baba5ea5abe5
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-03-04 20:21:53 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 pins.onPulsed(DigitalPin.P2, PulseValue.Low, function () {
-    Encoder += 1
-    if (Encoder >= Minuten * (191 / 3)) {
+    EncoderImpulse += 1
+    if (EncoderImpulse >= EncoderMinuten * (191 / 3)) {
         if (!(bUhrstellen)) {
             motors.dualMotorPower(Motor.M0, 0)
+            o4digit.show((EncoderImpulse - E2) * 100 + Minuten60)
+            E2 = EncoderImpulse
             basic.setLedColors(0x00ff00, 0x000000, 0x000000, 5)
         }
     }
@@ -10,18 +12,27 @@ pins.onPulsed(DigitalPin.P2, PulseValue.Low, function () {
 function _ (Kommentar: string) {
 	
 }
-let Encoder = 0
+let Minuten60 = 0
+let E2 = 0
+let EncoderImpulse = 0
 let bUhrstellen = false
-let Minuten = 0
-let o4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+let EncoderMinuten = 0
+let o4digit: grove.TM1637 = null
+o4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 o4digit.set(0)
+o4digit.point(true)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
-Minuten = 0
+EncoderMinuten = 0
 bUhrstellen = false
 basic.setLedColor(0xff0000)
 loops.everyInterval(60000, function () {
     if (!(bUhrstellen)) {
-        Minuten += 1
+        EncoderMinuten += 1
+        if (Minuten60 < 59) {
+            Minuten60 += 1
+        } else {
+            Minuten60 = 0
+        }
         motors.dualMotorPower(Motor.M0, 20)
         basic.setLedColors(0x000000, 0xb09eff, 0x000000)
     }
